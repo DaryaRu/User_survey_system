8a46e784ec115df724532236d4f60b30904694f4
DaryaRu <89119484+DaryaRu@users.noreply.github.com>
2021-11-26 18:27:55 +0300

    getting the list of surveys filtered by the current date

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from survey.viewsets.survey_viewset import SurveyViewSet
+
+router = routers.DefaultRouter()
+router.register(r'surveys', SurveyViewSet, basename='surveys')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
